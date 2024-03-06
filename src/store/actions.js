37c01fa32a23e161a689077180ede2f9fdce03bd
zhangqiang <zhang1234qiang@sina.com>
2024-03-06 23:02:16 +0800

    增加状态管理action方法

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,12 @@
 const actions={
-    
+    do({commit},data){
+        commit("COMMIT_TOKEN",data)
+    },
+    doOther(context){
+      context.commit("TAGES_LIST")
+    }
 }
+//在vue组件中使用
+//this.$store.dispatch("do",{data:data}) 或 this.$store.dispatch({type:"do"}) 
+//this.$store.dispatch("does")
 export default actions
